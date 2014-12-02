fbfdbd282e404745365259075c9312fa9ad914f1
zhouhaibing089 <zhouhaibing089@gmail.com>
2014-12-02 20:45:53 +0800

    spaces

--- src/formgen-basic.js
+++ src/formgen-basic.js
@@ -1,19 +1,19 @@
 define(['formgen-core'], function(require, exports, module) {
-	// the formgen-core module
-	var FG = require('formgen-core');
-	// the alias of document
-	var doc = document;
+    // the formgen-core module
+    var FG = require('formgen-core');
+    // the alias of document
+    var doc = document;
 
-	FG.registerHandler("hidden", hidden);
-	FG.registerHandler("text", text);
+    FG.registerHandler("hidden", hidden);
+    FG.registerHandler("text", text);
 
-	// just add attribute the disabled property to be true
-	var disabledFrozen = function() {
-		this.attr("disabled", true);
-	};
+    // just add attribute the disabled property to be true
+    var disabledFrozen = function() {
+        this.attr("disabled", true);
+    };
 
-	// get hidden input
-	function hidden(cfg, val, callback) {
+    // get hidden input
+    function hidden(cfg, val, callback) {
         var ret = $(doc.createElement("input"));
         // set the frozen function
         ret.frozen = disabledFrozen;
