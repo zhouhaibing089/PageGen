5a1fe5135f9aeb15fc8854e91eed5f0ef558bd53
zhouhaibing089 <zhouhaibing089@gmail.com>
2014-12-02 15:34:25 +0800

    indent with spaces

--- src/formgen.js
+++ src/formgen.js
@@ -3,98 +3,98 @@
  */
 define(['jquery', 'util'], function(require, exports, module) {
 
-	// module variable
-	/* extension point */
-	var handlers = {
-		"hidden": hidden,
-		"text": text,
-		"textarea": textarea,
-		"select": select,
-		"checkbox": checkbox
-	};
+    // module variable
+    /* extension point */
+    var handlers = {
+        "hidden": hidden,
+        "text": text,
+        "textarea": textarea,
+        "select": select,
+        "checkbox": checkbox
+    };
 
 
-	// FG variable 
-	var FG = function(config, value) {
-		var args = arguments;
-		this.config = args.length > 0 ? args[0] : {};
-		this.value = args.length > 1 ? args[1] : {};
-	};
+    // FG variable 
+    var FG = function(config, value) {
+        var args = arguments;
+        this.config = args.length > 0 ? args[0] : {};
+        this.value = args.length > 1 ? args[1] : {};
+    };
 
-	/* init a new instance of FormGenerator */
-	FG.init = function() {
-		return new FG(arguments);
-	};
+    /* init a new instance of FormGenerator */
+    FG.init = function() {
+        return new FG(arguments);
+    };
 
-	/* register a new handler for given type */
-	FG.registerHandler = function(type, handler) {
-		handlers[type] = handler;
-	};
-
-
-	// FGP variable
-	var FGP = FG.prototype;
-
-	/**
-	 * the first argument is the parent element where the generated form
-	 * will be inserted into.
-	 */
-	FGP.build = function(p) {
-		if (!(p instanceof jQuery)) {
-			p = $(p);
-		}
-		var form = $("<form>");
-		/* insert into dom */
-		p.append(form);
-		/* add form attributes */
-		form.attr("id", config.formId);
-		form.addClass(config.formClass);
-		form.attr("method", config.method);
-		form.attr("action", config.action);
-	};
-
-	/**
-	 * check the data integrity
-	 */
-	FGP.check = function(callback) {
-
-	};
+    /* register a new handler for given type */
+    FG.registerHandler = function(type, handler) {
+        handlers[type] = handler;
+    };
 
 
-	/**
-	 * get the data in form
-	 */
-	FGP.data = function(callback) {
+    // FGP variable
+    var FGP = FG.prototype;
 
-	}
+    /**
+     * the first argument is the parent element where the generated form
+     * will be inserted into.
+     */
+    FGP.build = function(p) {
+        if (!(p instanceof jQuery)) {
+            p = $(p);
+        }
+        var form = $("<form>");
+        /* insert into dom */
+        p.append(form);
+        /* add form attributes */
+        form.attr("id", config.formId);
+        form.addClass(config.formClass);
+        form.attr("method", config.method);
+        form.attr("action", config.action);
+    };
 
+    /**
+     * check the data integrity
+     */
+    FGP.check = function(callback) {
 
-	FGP.submit = function(callback, param) {
-		var data = $.extend(data(), param);
-
-	}
-
-	function hidden(cfg, val) {
-
-	}
-
-	function text(cfg, val) {
-
-	}
-
-	function textarea(cfg, val) {
-
-	}
-
-	function select(cfg, val) {
-
-	}
-
-	function checkbox(cfg, val) {
-
-	}
+    };
 
 
+    /**
+     * get the data in form
+     */
+    FGP.data = function(callback) {
 
-	return FG;
+    }
+
+
+    FGP.submit = function(callback, param) {
+        var data = $.extend(data(), param);
+
+    }
+
+    function hidden(cfg, val) {
+
+    }
+
+    function text(cfg, val) {
+
+    }
+
+    function textarea(cfg, val) {
+
+    }
+
+    function select(cfg, val) {
+
+    }
+
+    function checkbox(cfg, val) {
+
+    }
+
+
+
+    return FG;
 });
--- src/util.js
+++ src/util.js
@@ -1,40 +1,40 @@
 define(function(require, exports, module) {
 
-	/**
-	 * the getValue mehtod
-	 */
-	if (({}.getValue) === undefined) {
-		
-		Object.prototype.getValue = function(path) {
-			var obj;
-			
-			if (this instanceof String) {
-				try {
-					obj = JSON.parse(this);
-				} catch(x) {
-					return null;
-				}
-			} else {
-				obj = this;
-			}
+    /**
+     * the getValue mehtod
+     */
+    if (({}.getValue) === undefined) {
+        
+        Object.prototype.getValue = function(path) {
+            var obj;
+            
+            if (this instanceof String) {
+                try {
+                    obj = JSON.parse(this);
+                } catch(x) {
+                    return null;
+                }
+            } else {
+                obj = this;
+            }
 
-			if (path === "") {
-				return null;
-			}
+            if (path === "") {
+                return null;
+            }
 
-			var index = path.indexOf(".");
+            var index = path.indexOf(".");
 
-			if (index === -1) {
-				return obj[path];
-			}
+            if (index === -1) {
+                return obj[path];
+            }
 
-			var name = path.substr(0, index);
-			path = path.substr(index + 1);
+            var name = path.substr(0, index);
+            path = path.substr(index + 1);
 
-			return obj[name].getValue(path);
-		}
-	}
+            return obj[name].getValue(path);
+        }
+    }
 
-	return {};
+    return {};
 
 });
